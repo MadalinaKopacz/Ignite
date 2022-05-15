e67fe3a16940036cfd4f4c6dd70a7827e25aeca8
matei_nastase <nastasematei@gmail.com>
2022-05-15 14:16:56 +0300

    Database, CRUD on Activities, Generate Quizz view

---
+++ ignite/activities/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [path('create', views.create_activity),
+               path('get', views.get_all_activities),
+               path('<id>', views.get_by_id),
+               path('<id>/delete', views.delete_activity),
+               path('<id>/update', views.update_activity)
+]
