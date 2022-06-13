c8af3b294278c07fae4bebae064ff0211be12bb5
Madalina Kopacz <madalina.elena.kopacz@gmail.com>
2022-06-13 22:43:21 +0300

    add 8 unit tests

--- ignite/accounts/tests.py
+++ ignite/accounts/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
-
+from accounts.models import User
+from django.urls import reverse
+from start_page import *
 # Create your tests here.
+
+class AccountsTest(TestCase):
+    def setUp(self):
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')
+        test_user1.save()
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('start_page:get_start_page'))
+
+
+        self.assertEqual(str(response.context['user']), 'testuser1')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'global/start_page.html')
